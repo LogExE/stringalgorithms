b6f43eed18a10083b4198f994aff39d43868d565
LogXx <awesome149712@gmail.com>
2021-11-15 13:13:53 +0400

    Update vagner-fisher.py

--- vagner-fisher.py
+++ vagner-fisher.py
@@ -12,8 +12,6 @@ def levenstein_diff(s1, s2):
         for j in range(1, m + 1):
             t = 1 if s1[i - 1] != s2[j - 1] else 0
             D[i][j] = min(D[i - 1][j] + 1, D[i][j - 1] + 1, D[i - 1][j - 1] + t)
-    for x in D:
-        print(x)
     return D[n][m]
 
 if __name__ == "__main__":
